d7008ba142b9102cd0ead54bc29d078f619328e8
kayle <215031463@qq.com>
2017-09-01 19:51:03 +0800

    finish AmapComponent

--- src/app/map/gaode-map/amap/amap.component.ts
+++ src/app/map/gaode-map/amap/amap.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+declare const AMap: any;
+
 @Component({
   selector: 'app-amap',
   templateUrl: './amap.component.html',
@@ -10,6 +12,23 @@ export class AmapComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+    let map  = new AMap.Map('gaodemap-container');
+    map.plugin('AMap.Geolocation', () => {
+      let geolocation = new AMap.Geolocation({
+        enableHighAccuracy: true,
+        timeout: 10000,
+        maximumAge: 0,
+        convert: true,
+        showButton: true,
+        buttonPosition: 'LB',
+        buttonOffset: new AMap.Pixel(10, 20),
+        showMarker: true,
+        showCircle: true,
+        panToLocation: true,
+        zoomToAccuracy: true
+      });
+      map.addControl(geolocation);
+    });
   }
 
 }
--- src/app/map/gaode-map/gaode-map.component.ts
+++ src/app/map/gaode-map/gaode-map.component.ts
@@ -1,9 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 
+import { AmapComponent } from './amap/amap.component';
+import { flyInOut } from '../../animations/fly-in-out';
+
 @Component({
   selector: 'app-gaode-map',
   templateUrl: './gaode-map.component.html',
-  styleUrls: ['./gaode-map.component.scss']
+  styleUrls: ['./gaode-map.component.scss'],
+  animations: [ flyInOut ]
 })
 export class GaodeMapComponent implements OnInit {
 
